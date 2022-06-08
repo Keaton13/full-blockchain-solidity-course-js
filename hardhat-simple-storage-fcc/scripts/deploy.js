1f19b1d6ba08e17851790ef71511d428cb950964
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2022-06-07 19:54:43 -0700

    Fixed etherscan api issue

--- hardhat-simple-storage-fcc/scripts/deploy.js
+++ hardhat-simple-storage-fcc/scripts/deploy.js
@@ -1,5 +1,5 @@
 // imports
-const { ethers, run } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 // async main
 async function main() {
@@ -12,9 +12,19 @@ async function main() {
     console.log(`Deployed contract to: ${simpleStorage.address}`)
     // what happens when we deploy to our hardhat network?
     if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+      console.log("Waiting for block confirmations...")
       await simpleStorage.deployTransaction.wait(6);
       await verify(simpleStorage.address, []);
     }
+
+    const currentValue = await simpleStorage.retrieve();
+    console.log(`Current Value is: ${currentValue}`)
+
+    // Update the current value
+    const transactionResponse = await simpleStorage.store(7);
+    await transactionResponse.wait(1);
+    const updatedValue = await simpleStorage.retrieve();
+    console.log(`Updated value is: ${updatedValue}`)
 }
 
 async function verify(contractAddress, args) {
