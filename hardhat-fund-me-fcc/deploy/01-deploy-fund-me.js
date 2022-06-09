aa2e571b0bc8b18ec0b61b28e8ab8bdec091cd4d
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2022-06-09 14:21:57 -0700

    Creating tests

--- hardhat-fund-me-fcc/deploy/01-deploy-fund-me.js
+++ hardhat-fund-me-fcc/deploy/01-deploy-fund-me.js
@@ -1,15 +1,6 @@
-// imports
-// main function
-// calling of main function
-
-// function deployFunc() {
-//     console.log("Hi!")
-// }
-
-// module.exports.default = deployFunc;
-
-const { networkConfig } = require("../helper-hardhat-config")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 const {network} = require("hardhat");
+const { verify } = require("../utils/verify");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
@@ -19,16 +10,32 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     // if chainId is X use y
     // if chainId is Z use A
 
-    const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+    let ethUsdPriceFeedAddress
+    if (chainId == 31337) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
 
     // if the contract doesn't exist, we deploy a minimal version of 
     // for our local testing
 
+
     // well what happens when we want to change chains?
     // when going for localhost or hardhat network we want to use a mock
+    const args = [ethUsdPriceFeedAddress]
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: [address], // put price feed address
+        args: args, // put price feed address
         log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
     })
+
+    if(!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY){
+        process.env.ETHERSCAN_API_KEY
+    } else {
+        await verify(fundMe.address, args);
+    }
+    log("---------------------------------------------")
 }
